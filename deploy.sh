419044bd93fd8f14c0cbabb95cfbaba9bcca82b9
i.chelyshkin <i.chelyshkin@corp.mail.ru>
2018-02-15 20:03:48 +0300

    add django init state

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ states_path="/root/blanks/environments"
 
 git clone git@github.com:la-square/blanks.git
 $states_path/system-centOS7-installer.sh 	-n burokrat
-$states_path/venv-python35-prepair.sh    	-n burokrat
+$states_path/venv-django-prepair    		-n burokrat
 $states_path/psql-init-db.sh             	-n burokrat_db	-u hotdog	-p 12345
 $states_path/gunicorn-service-init.sh     	-n burokrat
 $states_path/nginx-basic-config.sh
