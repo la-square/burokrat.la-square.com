1e240cc57dff4f7fb823fdb009bd09101ae59b2d
i.chelyshkin <i.chelyshkin@corp.mail.ru>
2018-02-15 19:53:19 +0300

    init deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#Script deploy burokrat application
+states_path="/root/blanks/environments"
+
+git clone git@github.com:la-square/blanks.git
+$states_path/system-centOS7-installer.sh 	-n burokrat
+$states_path/venv-python35-prepair.sh    	-n burokrat
+$states_path/psql-init-db.sh             	-n burokrat_db	-u hotdog	-p 12345
+$states_path/gunicorn-service-init.sh     	-n burokrat
+$states_path/nginx-basic-config.sh
+$states_path/nginx-http-server.sh         	-n burokrat
+rm -rf /root/blanks
