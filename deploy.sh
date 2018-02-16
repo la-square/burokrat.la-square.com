b55941c2609d8e57af5a050070d26eabbc82beb1
i.chelyshkin <i.chelyshkin@corp.mail.ru>
2018-02-16 14:33:44 +0300

    modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 #Script deploy burokrat application
 states_path="/root/blanks-deploy"
 
-git clone git@github.com:la-square/blanks.git
+git clone git@github.com:la-square/blanks-deploy.git
 $states_path/system/centOS7-installer.sh 	 	-n burokrat
 $states_path/python35/django-prepair.sh    		-n burokrat
 $states_path/postgres/psql-init-db.sh           -n burokrat_db	-u hotdog	-p 12345
