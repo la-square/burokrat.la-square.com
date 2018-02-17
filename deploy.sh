1d2bef1528b1d45c8745bb1ced6fef9be7ef4b3e
i.chelyshkin <i.chelyshkin@corp.mail.ru>
2018-02-17 12:00:29 +0300

    add hostname param

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ $states_path/python35/django-prepair.sh    		-n burokrat
 $states_path/postgres/psql-init-db.sh           -n burokrat_db	-u hotdog	-p 12345
 $states_path/gunicorn/gunicorn-service-init.sh  -n burokrat
 $states_path/nginx/nginx-basic-config.sh
-$states_path/nginx/nginx-http-server.sh         -n burokrat
+$states_path/nginx/nginx-http-server.sh         -n burokrat 	-h burokrat.la-square.com
 rm -rf $states_path
